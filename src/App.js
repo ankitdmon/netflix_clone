b91fbbc2e1115fe1068e224d9f60364fc97f5187
ankitdmon <ankitmishra2108@gmail.com>
2022-11-13 22:53:17 +0530

    testing Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+
+      <Router>
+        <Switch>
+          <Route path= "/test">
+            <h1>
+              Wow! Ankit
+            </h1>
+          </Route>
+          <Route exact path= "/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
