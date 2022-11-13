22ca79e7fdf31ba2d6ceae9a6b9c66db991688f2
ankitdmon <ankitmishra2108@gmail.com>
2022-11-13 22:11:54 +0530

    hosting On Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
